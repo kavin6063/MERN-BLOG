4aaf3bd883f64775178f705d5f42e88299e20158
Kavin <kavinkarthickeyan315@gmail.com>
2024-10-23 11:32:19 +0530

    added flowbite & header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Projects from "./pages/Projects";
 import DashBoard from "./pages/Dashboard";
+import Header from "./components/Header";
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
